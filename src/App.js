683ef0f5c2ede9fc784a139462a23f10dc25056d
tohin222 <vampirehaunted24@gmail.com>
2019-11-13 15:26:14 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createStore } from 'redux'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Front to Back !</h1>
+    </div>
+  );
+}
+
+export default App;
